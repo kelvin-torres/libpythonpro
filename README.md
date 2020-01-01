9fbb609aa2ce78600a3c88986b581dc4733c0752
Kelvin Torres <kelvinlgtorres@gmail.com>
2020-01-01 13:24:41 -0500

    Adicionado flake 8

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # libpythonpro
 
 Arquivo de teste de virtual env, library e requests.
+
+Para instalar:
+
+```console
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements-dev.txt
+```
+
+Para confierir qualidade de codigo:
+'''console
+flake8
+
+'''
--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -9,9 +9,10 @@ def buscar_avatar(usuario):
     :return str com o link do avatar
     """
 
-    url=f"https://api.github.com/users/{usuario}"
+    url = f"https://api.github.com/users/{usuario}"
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     print(buscar_avatar('kelvin-torres'))
